db450007c3d6ad66fa5e6d9f3f4ffe63f49fb9f9
River Ann <maragriver@gmail.com>
2022-03-13 15:14:26 +0900

    add: routes as home, blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import Container from '../components/Container';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Container>
+      <span className={`font-bold`}>HOME</span>
+    </Container>
+  );
 }
 
 export default Home
